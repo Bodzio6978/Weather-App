3e8e3f25cb3788ebce653ea8dc114b679ba3e951
Bogumił Męcel <Bodziowaty6978@gmail.com>
2022-07-21 19:52:57 +0200

    finished implementing ui

--- app/src/main/java/com/gmail/bogumilmecel2/weatherapp/data/location/DefaultLocationTracker.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/weatherapp/data/location/DefaultLocationTracker.kt
@@ -6,57 +6,56 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationManager
+import android.util.Log
 import androidx.core.content.ContextCompat
 import com.gmail.bogumilmecel2.weatherapp.domain.location.LocationTracker
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
 import kotlin.coroutines.resume
 
+@ExperimentalCoroutinesApi
 class DefaultLocationTracker @Inject constructor(
     private val locationClient: FusedLocationProviderClient,
     private val application: Application
-):LocationTracker {
+) : LocationTracker {
 
-    @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun getCurrentLocation(): Location? {
         val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(
             application,
             Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
-
         val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
             application,
             Manifest.permission.ACCESS_COARSE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
 
-        val locationManager = application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationManager =
+            application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
                 locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-
-        if (!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled){
+        if (!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled) {
             return null
         }
 
         return suspendCancellableCoroutine { cont ->
             locationClient.lastLocation.apply {
-                if (isComplete){
-                    if (isSuccessful){
+                if (isComplete) {
+                    if (isSuccessful) {
                         cont.resume(result)
-                    }else{
+                    } else {
                         cont.resume(null)
                     }
                     return@suspendCancellableCoroutine
                 }
-                addOnSuccessListener {
-                    cont.resume(it)
+                addOnSuccessListener { location ->
+                    cont.resume(location)
                 }
-
                 addOnFailureListener {
                     cont.resume(null)
                 }
-
                 addOnCanceledListener {
                     cont.cancel()
                 }
--- app/src/main/java/com/gmail/bogumilmecel2/weatherapp/presentation/WeatherViewModel.kt
+++ app/src/main/java/com/gmail/bogumilmecel2/weatherapp/presentation/WeatherViewModel.kt
@@ -1,5 +1,6 @@
 package com.gmail.bogumilmecel2.weatherapp.presentation
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -9,6 +10,7 @@ import com.gmail.bogumilmecel2.weatherapp.domain.location.LocationTracker
 import com.gmail.bogumilmecel2.weatherapp.domain.repository.WeatherRepository
 import com.gmail.bogumilmecel2.weatherapp.domain.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -23,16 +25,16 @@ class WeatherViewModel @Inject constructor(
     )
     private set
 
-    fun loadWeatherInfo(){
+    fun loadWeatherInfo() {
         viewModelScope.launch {
             state = state.copy(
                 isLoading = true,
                 error = null
             )
-
-            locationTracker.getCurrentLocation()?.let {location ->
-
-                when(val result = repository.getWeatherData(location.latitude, location.longitude)){
+            delay(10000)
+            locationTracker.getCurrentLocation()?.let { location ->
+                when (val result =
+                    repository.getWeatherData(location.latitude, location.longitude)) {
                     is Resource.Success -> {
                         state = state.copy(
                             weatherInfo = result.data,
@@ -49,11 +51,11 @@ class WeatherViewModel @Inject constructor(
                     }
                 }
             } ?: kotlin.run {
-                state = state.copy(
-                    isLoading = false,
-                    error = "Could not get a location. Make sure to grant permission and enable GPS."
-                )
-            }
+                    state = state.copy(
+                        isLoading = false,
+                        error = "Couldn't retrieve location. Make sure to grant permission and enable GPS."
+                    )
+                }
         }
     }
     
